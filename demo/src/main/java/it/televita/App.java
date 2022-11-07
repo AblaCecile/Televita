36244c8a3c788c7458f8732a0ff13c9a846dfcd2
Abla Cecile Daklu <ablacecile.daklu@TVA.TS>
2022-11-07 14:52:31 +0100

    primo commit

---
+++ demo/src/main/java/it/televita/App.java
@@ -0,0 +1,12 @@
+package it.televita;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
