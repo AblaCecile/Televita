410be4e9dd93c4a512838b698896ff1ab4b88c61
Abla Cecile Daklu <ablacecile.daklu@TVA.TS>
2022-11-09 15:37:02 +0100

    non ho fatto modifica al codice

--- demo/src/main/java/it/televita/App.java
+++ demo/src/main/java/it/televita/App.java
@@ -1,12 +1,29 @@
 package it.televita;
-/**
- * Hello world!
- *
- */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class App 
 {
+    private final String url = "jdbc:postgresql://localhost:54321/primodb";
+    private final String user = "postgres";
+    private final String password = "postgres";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.connect();
+        System.out.println( "Connessione ok" );
     }
 }
